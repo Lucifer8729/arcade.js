554c07a53e5a43cd30484958dd7ac5ea71b689a3
Saurav Kumar <72292326+Lucifer8729@users.noreply.github.com>
2021-09-17 21:18:28 +0530

    added typing effect

--- script.js
+++ script.js
@@ -1,1 +1,74 @@
-console.log("hello world");
+class TypeWriter {
+  constructor(textElement, words, wait) {
+    this.textElement = textElement;
+    this.words = words;
+    this.wait = wait;
+    this.txt = "";
+    this.i = 0;
+    this.isDeleting = false;
+
+    this.type();
+  }
+
+  type() {
+    const current = this.i % this.words.length;
+    const fullTxt = this.words[current];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.textElement.textContent = this.txt;
+
+    let typeSpeed = 300;
+    if (this.isDeleting) typeSpeed /= 2;
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+      typeSpeed = this.wait;
+
+      if (this.i === 0) blinkCursor();
+      if (this.i == 1) {
+        const cursor = document.querySelector(".msg-cursor");
+        const blink = setInterval(() => {
+          cursor.classList.toggle("blink");
+        }, 500);
+
+        return;
+      }
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.i = 1;
+      this.isDeleting = false;
+      typeSpeed = 300;
+    }
+
+    console.log(this.txt);
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+const blinkCursor = () => {
+  const cursor = document.querySelector(".msg-cursor");
+  const blink = setInterval(() => {
+    cursor.classList.toggle("blink");
+  }, 500);
+
+  setTimeout(() => {
+    clearInterval(blink);
+  }, 3000);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  blinkCursor();
+  setTimeout(init, 3500);
+});
+
+const init = () => {
+  const textElement = document.querySelector(".home-msg-txt");
+  const words = JSON.parse(textElement.getAttribute("data-words"));
+  const wait = parseInt(textElement.getAttribute("data-wait"));
+
+  new TypeWriter(textElement, words, wait);
+};
