9acd211266341f599307ef271aaf95c2b09c8ae0
Aniket Agarwal <aniketagarwal57@gmail.com>
2021-09-19 22:31:16 +0530

    Update GAME_DATA.js

--- scripts/GAME_DATA.js
+++ scripts/GAME_DATA.js
@@ -67,7 +67,7 @@ export const GAME_DATA = [
         image: "assets/gameImages/tic-tac-toe.png",
       },
     ],
-    profilePic: "assets/devImages/profilePic.png",
+    profilePic: "assets/devImages/anurag.jpg",
     aboutPic: "assets/devImages/aboutPic.png",
     about: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
             industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
